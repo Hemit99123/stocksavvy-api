a7b6a5c98231299dd8404bc0a835f34b93d257da
Hemit Patel <hemitvpatel@gmail.com>
2025-01-30 14:32:09 -0500

    added create question for forum

--- src/controllers/forum.controller.ts
+++ src/controllers/forum.controller.ts
@@ -1,5 +1,43 @@
+import { Request, Response } from "express";
+import handleError from "../utils/handleError.js";
+import { forum } from "../schema.js";
+import { db } from "../utils/db.js";
+
 const forumController = {
-    
-}
+    create: async (req: Request, res: Response) => {
+        const body = req.body;
+        const question = body.question;
+        const email = req.session.user?.email;
+
+        if (!question || !email) {
+            return res.status(400).json({
+                message: "Question and email are required",
+            });
+        }
+
+        try {
+            await db
+                .insert(forum)
+                .values({
+                    question,
+                    email,
+                    createdAt: new Date().toISOString() // Convert to ISO string format
+                });
+
+            res.status(201).json({
+                message: "Forum question created successfully",
+            });
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                handleError(res, error);
+            } else {
+                res.status(500).json({
+                    message: "An unexpected error occurred",
+                    error: "unknown-error",
+                });
+            }
+        }
+    }
+};
 
 export default forumController;
