8c3d1f52a112f6d12b3b98fb2eae9fda9200c060
Hemit Patel <hemitvpatel@gmail.com>
2025-01-30 14:34:41 -0500

    added route for create questions

--- src/routes/forum.route.ts
+++ src/routes/forum.route.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import forumController from "../controllers/forum.controller.js";
+
 const router = express.Router();
 
+router.post("/create", forumController.create)
 
 export default router;
