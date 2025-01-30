dc80eb8571fe6ff275e804bcffe891cd4ec018a9
Hemit Patel <hemitvpatel@gmail.com>
2025-01-30 14:24:03 -0500

    added the forum schema data

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,16 @@
-import { text, pgTable } from "drizzle-orm/pg-core";
+import { text, pgTable, date } from "drizzle-orm/pg-core";
 
-// user table schema remains the same
 export const user = pgTable("user", {
-  email: text("email").primaryKey(), // make email the primary key
+  email: text("email").primaryKey(),
   name: text("name").notNull(),
   googleid: text("googleid").notNull(),
+});
+
+export const forum = pgTable("forum", {
+  question: text("question").notNull(),
+  email: text("email")
+    .references(() => user.email) // references the email in the user table (foreign key)
+    .notNull(), 
+  createdAt: date("createdAt")
+    .default('CURRENT_DATE') // Set the default value to current date
 });
