e8fe6257dd403a697478cfd6ed60e2f861d83986
Hemit Patel <hemitvpatel@gmail.com>
2025-01-29 20:46:59 -0500

    some config fix

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
-    transform: {
-      '^.+\\.ts$': 'babel-jest',
-    },
-    testEnvironment: 'node',
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', // Transforms TypeScript files using ts-jest
+    '^.+\\.js$': 'babel-jest', // Transforms JS files using Babel
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!your-package-to-transform|other-package-to-transform).*/', // Add package names if you need to transpile from node_modules
+  ],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'], // Recognizes .ts files
+};
